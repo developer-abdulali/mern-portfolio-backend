9b6979a5d4032144963d97ed096d1090f4423335
developer-abdulali <ab.ali.soomro@gmail.com>
2024-12-06 01:14:24 +0500

    added changes

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ cloudinary.v2.config({
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Backend Portfolio Server is running...");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
